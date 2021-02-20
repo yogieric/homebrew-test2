7965293d1b8cf370d2dcf1c919f8621f001b19d9
Eric (iCloud) Brown <ericbrown@khanacademy.com>
2021-02-19 16:12:58 -0800

    add proto

---
+++ Formula/protobuf@3.4.rb
@@ -0,0 +1,22 @@
+class ProtobufAT34 < Formula
+  desc "Protocol buffers (Google's data interchange format)"
+  homepage "https://github.com/protocolbuffers/protobuf/"
+  license "BSD-3-Clause"
+
+  if OS.mac?
+    url "https://github.com/protocolbuffers/protobuf/releases/download/v3.4.0/protoc-3.4.0-osx-x86_64.zip"
+    sha256 "8df109526ad6588f204fdeb7bc4843eb5f3246390c2f21b563473d43cb70e890"
+  else
+    url "https://github.com/protocolbuffers/protobuf/releases/download/v3.4.0/protoc-3.4.0-linux-x86_64.zip"
+    sha256 "e4b51de1b75813e62d6ecdde582efa798586e09b5beaebfb866ae7c9eaadace4"
+  end
+
+  def install
+    prefix.install Dir["*"]
+  end
+
+  test do
+    # TODO: Add some tests
+    system "true"
+  end
+end
